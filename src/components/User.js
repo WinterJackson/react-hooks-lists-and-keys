8b5da395ee0ffcf4c388bc8a08d79d48be89e658
WinterJackson <winterjacksonwj@gmail.com>
2023-07-25 09:03:42 +0300

    Completed

---
+++ src/components/User.js
@@ -0,0 +1,32 @@
+const user = {
+    name: "John Jackson Winter",
+    city: "Nairobi",
+    bio: "Coding is awsome",
+    color: "firebrick",
+    links: {
+        github: "https://github.com/WinterJackson",
+        linkedin: "https://www.linkedin.com/in/winter-jackson-454843178/",
+    },
+    projects: [
+        {
+            id: 1,
+            name: "Reciplease",
+            about: "A recipe tracking app",
+            technologies: ["Rails", "Bootstrap CSS"],
+        },
+        {
+            id: 2,
+            name: "Kibbles N Bitz",
+            about: "Tinder for dogs",
+            technologies: ["React", "Redux"],
+        },
+        {
+            id: 3,
+            name: "Alienwares",
+            about: "Etsy for aliens",
+            technologies: ["React", "Redux", "Rails"],
+        },
+    ],
+    };
+    
+    export default user;
